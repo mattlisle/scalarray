827c1dae4bc596bee57f08ef43fe8845ca59c664
mattlisle <matthew.j.lisle@gmail.com>
2020-04-04 15:56:34 -0700

    first pass at broadcasting

--- src/main/scala/scalarray/ArrayNdOps.scala
+++ src/main/scala/scalarray/ArrayNdOps.scala
@@ -2,7 +2,9 @@ package scalarray
 
 import scala.annotation.tailrec
 import scala.collection.AbstractIterator
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.reflect.ClassTag
+import scalarray.ArrayNdOps.BroadcastException
 
 /**
   * N-dimensional array implementation class
@@ -53,46 +55,7 @@ trait ArrayNdOps[A] extends ArrayNdGenericOps[A] {
   def lastOption: Option[A] = if (isEmpty) None else Some(last)
 
   /** Iterator that returns elements in row-major order regardless of whether the array is transposed */
-  def iterator: Iterator[A] =  new AbstractIterator[A] {
-    private var counter = 0
-    private var idx = 0
-    private val maxCount = elements.length
-    private lazy val indices = Array.fill[Int](shape.length.max(1))(idx)
-
-    private lazy val products = {
-      val reverse = shape.reverse
-      (shape.length until 0 by -1).map(n => reverse.drop(n).product)
-    }
-
-    private def updateState(): Unit = {
-      @tailrec
-      def update1dIdx(dim: Int): Unit = if (indices(dim) < shape(dim) - 1) {
-        indices(dim) += 1
-        idx += products.slice(dim, dim + 2).sum
-        if (idx >= elements.length) {
-          idx = idx - elements.length
-        }
-      } else {
-        indices(dim) = 0
-        update1dIdx(dim - 1)
-      }
-
-      counter += 1
-      if (transposed && hasNext) {
-        update1dIdx(indices.length - 1)
-      } else {
-        idx += 1
-      }
-    }
-
-    override def hasNext: Boolean = counter < maxCount
-
-    override def next(): A = {
-      val element = elements(idx)
-      updateState()
-      element
-    }
-  }
+  def iterator: Iterator[A] = if (transposed) new TransposedIterator else new StandardIterator
 
   /**
     * Map operation for n-dimensional arrays, which must return another n-dimensional array of the same shape
@@ -119,5 +82,120 @@ trait ArrayNdOps[A] extends ArrayNdGenericOps[A] {
     * @param f function to apply to each element
     */
   def foreach(f: A => Unit): Unit = iterator.foreach(f)
+
+
+  /** Iterator for a standard (non-transposed) `ArrayNd` */
+  private class StandardIterator extends AbstractIterator[A] {
+    private var counter = 0
+    private var idx = 0
+    private val maxCount = elements.length
+
+    override def hasNext: Boolean = counter < maxCount
+
+    override def next(): A = {
+      val element = elements(idx)
+      counter += 1
+      idx += 1
+      element
+    }
+  }
+
+  /** Iterator for a transposed `ArrayNd` */
+  private class TransposedIterator extends AbstractIterator[A] {
+    private var counter = 0
+    private var idx = 0
+    private val maxCount = elements.length
+    private val indices = Array.fill[Int](shape.length.max(1))(idx)
+
+    private val products: Array[Int] = {
+      val reverse = shape.reverse
+      (shape.length until 0 by -1).map(n => reverse.drop(n).product)
+      }.toArray
+
+
+    private def updateState(): Unit = {
+      @tailrec
+      def update1dIdx(dim: Int): Unit = if (indices(dim) < shape(dim) - 1) {
+        indices(dim) += 1
+        idx += products(dim) + products(dim + 1)
+        if (idx >= elements.length) {
+          idx = idx - elements.length
+        }
+      } else {
+        indices(dim) = 0
+        update1dIdx(dim - 1)
+      }
+
+      counter += 1
+      if (hasNext) update1dIdx(indices.length - 1)
+    }
+
+    override def hasNext: Boolean = counter < maxCount
+
+    override def next(): A = {
+      val element = elements(idx)
+      updateState()
+      element
+    }
+  }
+
+  /** Iterator for non-transposed `ArrayNd` that's broadcast from `oldShape` to `newShape` */
+  private class StandardBroadcastIterator(val that: ArrayNd[A]) extends AbstractIterator[A] {
+
+    def getBroadcastShape: Seq[Int] = {
+      val thisLength = shape.length
+      val thatLength = that.shape.length
+
+      val (thisPadded, thatPadded) = if (thisLength > thatLength) {
+        (shape, that.shape.padTo(thisLength, 1))
+      } else {
+        (shape.padTo(thatLength, 1), that.shape)
+      }
+
+      val broadcastShape = new ListBuffer[Int]
+
+      @tailrec
+      def compareDims(x: Seq[Int], y: Seq[Int]): Unit = (x, y) match {
+        case (x :: xs, y :: ys) =>
+          if (x != y && (x != 1 || y != 1)) {
+            throw new BroadcastException(shape, that.shape)
+          } else {
+            broadcastShape += x.max(y)
+            compareDims(xs, ys)
+          }
+        case _ =>
+      }
+
+      compareDims(thisPadded, thatPadded)
+      broadcastShape.toSeq
+    }
+
+    def getBounds(shp: Seq[Int]): Array[Int] = {
+      val buf = new ArrayBuffer[Int]
+      buf += 0
+
+      buf.toArray
+    }
+
+    private val broadcastShape = getBroadcastShape
+    private val thisIterator = iterator
+    private val thatIterator = that.iterator
+
+    private var counter = 0
+    private var idx = 0
+    private val maxCount = broadcastShape.product
+    private val indices = Array.fill[Int](broadcastShape.length)(idx)
+
+    override def hasNext: Boolean = ???
+
+    override def next(): A = ???
+  }
+
+}
+
+object ArrayNdOps {
+
+  class BroadcastException(shape1: Seq[Int], shape2: Seq[Int])
+    extends IllegalArgumentException(s"Cannot broadcast array of shape $shape2 into $shape1")
 
 }
