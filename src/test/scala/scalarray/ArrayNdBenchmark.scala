f9652a86b82d4bcb072f67ccf7e504fe67ec5b09
mattlisle <matthew.j.lisle@gmail.com>
2020-04-16 21:04:13 -0700

    change benchmark to measure shape

--- src/test/scala/scalarray/ArrayNdBenchmark.scala
+++ src/test/scala/scalarray/ArrayNdBenchmark.scala
@@ -53,14 +53,18 @@ object ArrayNdBenchmark extends Bench.ForkedTime {
   }
 
   performance of "broadcasted array-array multiplication" in {
-    val numElements = Gen.range("elements")(500, 1000, 100)
-    val shapes = numElements.map { n =>
-      val x = n / 10
-      val y = 10
-      (Seq(x, y, 1, 1), Seq(1, 1, x, y))
-    }
-    val operands = shapes.map {
-      pair =>
+    val indices = Gen.range("dimensionality")(0, 4, 1)
+    val shapes = Seq(
+      Seq(1000, 1),
+      Seq(100, 10, 1, 1),
+      Seq(10, 10, 10, 1, 1, 1),
+      Seq(10, 10, 5, 2, 1, 1, 1, 1),
+      Seq(10, 5, 2, 5, 2, 1, 1, 1, 1, 1)
+    )
+    val operands = indices.map {
+      idx =>
+        val shape = shapes(idx)
+        val pair = (shape, shape.reverse)
         val arr = ArrayNd.fill[Int](pair._1.product)(1)
         (arr.reshape(pair._1: _*), arr.reshape(pair._2: _*))
     }
